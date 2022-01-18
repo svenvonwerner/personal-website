801cb456ff73e83fd08a1199ef5b04fe19ffebbe
Sven von Werner <sven.von.werner@gmail.com>
2022-01-18 12:12:08 +0100

    Install and cofigure storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import "../style.css";
+import "../base.css";
+import "../colors.css";
+import "../utility.css";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
